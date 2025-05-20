5f27d5196aab1e2ab81c3a1a602445a6c0b6b2f0
Robel Gebremeskel <rofikre@yahoo.com>
2025-05-20 19:43:23 -0400

    Configured Redux store and use React-redux hooks

---
+++ rtk-query-learning-project/src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../app/features/counter/counterSlice'
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <h1>{count}</h1>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
